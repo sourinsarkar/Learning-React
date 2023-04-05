8ddcf87610959982ee408297a912148991722026
Sourin <iamsourinsarkar@gmail.com>
2023-04-05 20:17:33 +0530

    Implemented Class Component the optimised and hard coded way

---
+++ src/HardCodedClass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HardCodedClass extends React.Component {
+    render() {
+        return (
+            <div>
+                Something
+            </div>
+        )
+    }
+}
+
+export default HardCodedClass;
